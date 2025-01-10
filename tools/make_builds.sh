294333748fa6ac806cc915d8a7c4848d9fe0a9d3
Andrey <pikachu_andrey@vk.com>
2025-01-11 01:48:12 +0900

    Upgrade Rust compiler (1.74.0)

--- tools/make_builds.sh
+++ tools/make_builds.sh
@@ -1,3 +1,5 @@
+set -e
+
 cmake -B cmake-build-debug -G Ninja . -DCMAKE_BUILD_TYPE=Debug
 cmake -B cmake-build-release -G Ninja . -DCMAKE_BUILD_TYPE=Release
 cmake -B cmake-build-release-opt -G Ninja . -DCMAKE_BUILD_TYPE=ReleaseOptimized
