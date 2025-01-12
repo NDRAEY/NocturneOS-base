9296b4434e091fed3a13070517cdd3147f29ed8f
Andrey <pikachu_andrey@vk.com>
2025-01-12 13:10:14 +0900

    [no ci] Add `doc` target to CMake

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -21,21 +21,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
-#[inline(never)]
-pub extern "C" fn io_test() {
-    let mut buf = String::new();
-
-    let mut file = std::fs::File::open("R:\\Zeraora.txt").unwrap();
-    file.read_to_string(&mut buf).unwrap();
-
-    println!("Data: [{}]", buf);
-}
-
+/// Main entry point for testing.
 #[no_mangle]
 #[inline(never)]
 pub extern "C" fn rust_main() {
     println!("Привет, {}!", "Rust");
-
-    io_test();
 }
