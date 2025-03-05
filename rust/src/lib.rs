b9ada7e0ce1100b933cd194a2e8501d90dd90fdb
Andrey <pikachu_andrey@vk.com>
2025-03-06 06:46:43 +0900

    Writing new heap

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,7 +3,7 @@
 
 extern crate alloc;
 
-use core::panic::PanicInfo;
+use core::{cell::OnceCell, panic::PanicInfo};
 
 pub mod audio;
 pub mod drv;
@@ -14,6 +14,7 @@ pub mod system;
 
 use noct_alloc::Allocator;
 pub use noct_logger::*;
+use noct_zeraora::Heap;
 
 #[global_allocator]
 static ALLOCATOR: Allocator = noct_alloc::Allocator;
@@ -24,6 +25,8 @@ fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
 }
+
+static mut GLOBAL_KERNEL_HEAP: OnceCell<noct_zeraora::Heap> = OnceCell::new();
 
 /// Main entry point for testing.
 #[no_mangle]
@@ -43,6 +46,16 @@ pub extern "C" fn rust_main() {
     //         qemu_ok!("{}", i);
     //     }
     // })
+
+    unsafe { GLOBAL_KERNEL_HEAP.set(Heap::with_entry_count(0x500_0000, 1024, 1024)) }.unwrap();
+    let mut heap: &mut Heap<'_> = unsafe { GLOBAL_KERNEL_HEAP.get_mut().unwrap() };
+    
+    let block1 = heap.alloc_no_map(0x12, 1).unwrap();
+    let block2 = heap.alloc_no_map(6, 16).unwrap();
+
+    qemu_note!("{:x} {:x}", block1, block2);
+
+    // let x = unsafe { GLOBAL_KERNEL_HEAP.get().unwrap() };
 
     noct_iso9660::fs_iso9660_init();
 }
