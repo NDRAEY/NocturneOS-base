0099ed62cdff65e6050124308a50eb1a76ad7e4d
NDRAEY <andrejpavlenko666@gmail.com>
2025-05-13 17:56:28 +0900

    Provide optimal method to generate badges*

--- tools/generate_warning_badge.sh
+++ tools/generate_warning_badge.sh
@@ -3,13 +3,3 @@
 count=$(python3 tools/warnanalyzer.py analyzer.txt)
 
 echo "{\"color\": \"yellow\", \"status\": \"$count\", \"subject\": \"Warnings\"}" > warning_count.txt
-
-ffmpeg -f lavfi -i "color=#cccc11:size=120x28" \
- -filter_complex "[v]drawtext=text=\'$count warnings\'
- :fontcolor=black
- :fontsize=14
- :x=(w-text_w)/2
- :y=8[out]" \
- -map '[out]' \
- -frames:v 1 \
- -y warn_badge.png
