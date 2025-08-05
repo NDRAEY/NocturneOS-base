e6329e916611d9d97c827230bd3ac8820b5d1bae
NDRAEY <andrejpavlenko666@gmail.com>
2025-08-05 21:58:07 +0900

    Add rdtsc and rdtsc32

--- kernel/include/sys/cpuinfo.h
+++ kernel/include/sys/cpuinfo.h
@@ -3,3 +3,19 @@
 #include <common.h>
 
 void get_cpu_brand(char* output, size_t* length_out);
+
+SAYORI_INLINE uint64_t rdtsc() {
+	uint32_t hi, lo;
+
+	__asm__ volatile("rdtsc" : "=a"(lo), "=d"(hi));
+	
+	return ( (uint64_t)lo)|( ((uint64_t)hi) << 32 );
+}
+
+SAYORI_INLINE uint32_t rdtsc32() {
+    uint32_t lo;
+
+    __asm__ volatile("rdtsc" : "=a"(lo));
+
+    return lo;
+}
